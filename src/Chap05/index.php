6a2b63afeefc0291b620e559f3c66a2b60235374
scada-josh <scada.josh@gmail.com>
2013-06-09 14:17:02 +0700

    setup Chapter 05

---
+++ src/Chap05/index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en" ng-app>
+<head>
+  <meta charset="utf-8">
+  <title>AngularJS : Communication with Server</title>
+  <!-- <link rel="stylesheet" href="css/app.css"> -->
+  <link rel="stylesheet" href="../../bootstrap/css/bootstrap.css">
+  <script src="../../angular-1.0.7/angular.js"></script>
+  <!-- <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.0.1/angular.min.js"></script> -->
+  <script src="./Controller/chap5Controller.js"></script>
+  
+</head>
+<body >
+  
+
+</body>
+</html>
+
